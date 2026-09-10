852782cd91403b84acd5b66a5721c030dcd8ceac
SANJII <himelovi1@gmail.com>
2026-09-11 02:01:54 +0600

    Update InfiniteInvClient.java

--- src/client/java/com/robsterad/infiniteinv/InfiniteInvClient.java
+++ src/client/java/com/robsterad/infiniteinv/InfiniteInvClient.java
@@ -1,5 +1,6 @@
 package com.robsterad.infiniteinv;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.robsterad.infiniteinv.config.InfiniteInvConfig;
 import com.robsterad.infiniteinv.gui.InfiniteInventoryOverlay;
 import com.robsterad.infiniteinv.network.SyncInventoryPayload;
@@ -7,15 +8,37 @@ import com.robsterad.infiniteinv.network.SyncUiPrefsPayload;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 
 @Environment(EnvType.CLIENT)
 public class InfiniteInvClient implements ClientModInitializer {
+    public static KeyMapping toggleKey;
 
     @Override
     public void onInitializeClient() {
         InfiniteInvConfig.load();
         InfiniteInventoryOverlay.register();
+
+        // Register Keybinding for toggling the inventory overlay (Default: 'I')
+        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.infiniteinv.toggle",
+                InputConstants.Type.KEYSYM,
+                InputConstants.KEY_I,
+                "category.infiniteinv"
+        ));
+
+        // Listen for key presses to toggle panel visibility
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (toggleKey.consumeClick()) {
+                if (Minecraft.getInstance().player != null) {
+                    InfiniteInventoryOverlay.togglePanel();
+                }
+            }
+        });
 
         ClientPlayNetworking.registerGlobalReceiver(SyncInventoryPayload.ID, (payload, context) -> {
             context.client().execute(() -> {

dee113aab574953139ffceb212387cc8ffd55be4
SANJII <himelovi1@gmail.com>
2026-09-11 23:09:56 +0600

    Update AbstractContainerScreenAccessor.java

--- src/main/java/com/robsterad/infiniteinv/mixin/AbstractContainerScreenAccessor.java
+++ src/main/java/com/robsterad/infiniteinv/mixin/AbstractContainerScreenAccessor.java
@@ -1,20 +1,25 @@
 package com.robsterad.infiniteinv.mixin;
 
+import com.robsterad.infiniteinv.gui.InfiniteInventoryOverlay;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AbstractContainerScreen.class)
-public interface AbstractContainerScreenAccessor {
-    @Accessor("leftPos")
-    int getLeftPos();
+@Mixin(value = AbstractContainerScreen.class, priority = 9999)
+public abstract class ScreenMixin {
 
-    @Accessor("topPos")
-    int getTopPos();
+    @Shadow protected int leftPos;
+    @Shadow protected int topPos;
+    @Shadow protected int imageWidth;
+    @Shadow protected int imageHeight;
 
-    @Accessor("imageWidth")
-    int getImageWidth();
-
-    @Accessor("imageHeight")
-    int getImageHeight();
+    @Inject(method = "render", at = @At("RETURN"))
+    private void onRenderOverlay(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        AbstractContainerScreen<?> screen = (AbstractContainerScreen<?>)(Object)this;
+        InfiniteInventoryOverlay.renderOverlay(screen, guiGraphics, mouseX, mouseY, delta, this.leftPos, this.topPos, this.imageWidth, this.imageHeight);
+    }
 }

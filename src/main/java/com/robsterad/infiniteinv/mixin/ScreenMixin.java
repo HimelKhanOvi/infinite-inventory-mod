75833b1860764bc9a7365f1e5e5211e703b2d0b5
SANJII <himelovi1@gmail.com>
2026-09-11 23:38:37 +0600

    Update ScreenMixin.java

--- src/main/java/com/robsterad/infiniteinv/mixin/ScreenMixin.java
+++ src/main/java/com/robsterad/infiniteinv/mixin/ScreenMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = AbstractContainerScreen.class, priority = 9999)
+@Mixin(AbstractContainerScreen.class)
 public abstract class ScreenMixin {
 
     @Shadow protected int leftPos;
@@ -18,7 +18,7 @@ public abstract class ScreenMixin {
     @Shadow protected int imageHeight;
 
     @Inject(method = "render", at = @At("RETURN"))
-    private void onRenderOverlay(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+    private void onRender(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         AbstractContainerScreen<?> screen = (AbstractContainerScreen<?>)(Object)this;
         InfiniteInventoryOverlay.renderOverlay(screen, guiGraphics, mouseX, mouseY, delta, this.leftPos, this.topPos, this.imageWidth, this.imageHeight);
     }

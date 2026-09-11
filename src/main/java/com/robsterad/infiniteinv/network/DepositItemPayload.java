a37bfe4b0edf0ba927d844076878d1353d46a33a
SANJII <himelovi1@gmail.com>
2026-09-12 00:47:03 +0600

    Update DepositItemPayload.java

--- src/main/java/com/robsterad/infiniteinv/network/DepositItemPayload.java
+++ src/main/java/com/robsterad/infiniteinv/network/DepositItemPayload.java
@@ -3,11 +3,11 @@ package com.robsterad.infiniteinv.network;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.item.ItemStack;
 
 public record DepositItemPayload(ItemStack stack) implements CustomPacketPayload {
-    public static final Type<DepositItemPayload> ID = new Type<>(ResourceLocation.fromNamespaceAndPath("infiniteinv", "deposit_item"));
+    public static final Type<DepositItemPayload> ID = new Type<>(Identifier.of("infiniteinv", "deposit_item"));
     public static final StreamCodec<RegistryFriendlyByteBuf, DepositItemPayload> CODEC = StreamCodec.composite(
         ItemStack.STREAM_CODEC, DepositItemPayload::stack,
         DepositItemPayload::new
